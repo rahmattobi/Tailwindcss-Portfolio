51d739c43251c99a71d70df8b59ed3da8a1a62c4
rahmattobi <tobytoby298@gmail.com>
2023-10-24 15:27:42 +0700

    update darkmode + fitur lainnya

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -8,7 +9,7 @@ module.exports = {
     },
     extend: {
       colors:{
-        primary: '#064e3b',
+        primary: '#096b51',
         dark: '#0f172a'
       },
 
